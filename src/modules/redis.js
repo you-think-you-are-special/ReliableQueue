c41798c6b97e99e323d0888292889298a83e84f2
Litvinov Alexander <alexander@codeordie.ru>
2016-01-30 23:50:42 +0300

    Add redis client support

--- src/modules/redis.js
+++ src/modules/redis.js
@@ -2,7 +2,10 @@ const P = require('bluebird')
 const _ = require('lodash')
 const redis = P.promisifyAll(require('redis'))
 
-module.exports.createClient = () => {
-  const options = _.isEmpty(arguments) ? [] : arguments
-  return redis.createClient.apply(redis, options)
-} 
+module.exports.createClient = (client) => {
+    if (client instanceof redis.RedisClient) {
+        return P.promisifyAll(client)
+    }
+    const options = _.isEmpty(arguments) ? [] : arguments
+    return redis.createClient.apply(redis, options)
+}
